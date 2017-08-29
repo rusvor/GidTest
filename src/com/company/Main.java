68f2187df0f11e37c90c681d1417574496cd08db
rusvor <rusya.vorobyov@gmail.com>
2017-08-29 23:07:33 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public void run()
+    {
+        System.out.println("test");
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+	
+    }
+}
